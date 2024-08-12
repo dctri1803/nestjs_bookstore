d426e0cb3ef680c34e858016436a6c1e6135e285
Đặng Cao Trí <no12spr@gmail.com>
2024-08-12 23:06:54 +0700

    bai 1

--- src/books-controller/books-controller.controller.ts
+++ src/books-controller/books-controller.controller.ts
@@ -1,23 +1,23 @@
 import { Controller, Delete, Get, Param, Post } from '@nestjs/common';
 
-@Controller('books-controller')
+@Controller('books')
 export class BooksControllerController {
-  @Get('/books')
+  @Get('')
   getAllBooks(): string {
     return 'This will return a list of all books';
   }
 
-  @Get('/book/:id')
+  @Get(':id')
   getBookById(@Param('id') id: string): string {
     return `This will return the details of the book with id: ${id}`;
   }
-  
-  @Post('/books')
+
+  @Post('')
   addBook(): string {
     return 'This will add a new book';
   }
 
-  @Delete('/books/id')
+  @Delete(':id')
   deleteBooks(@Param('id') id: string): string {
     return `This will add the book with id: ${id}`;
   }
